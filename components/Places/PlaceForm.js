a5bbe1d39f57bd88aa87a9426a468573530e9f16
chicodevbr <chicodevbr@gmail.com>
2022-04-17 18:36:21 -0300

    install image picker and permission

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,10 +1,43 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { ScrollView, View, Text, TextInput, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { GlobalStyles } from '../../constants/styles';
 
 export default function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
   return (
-    <View>
-      <Text>Place Form</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: GlobalStyles.colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: GlobalStyles.colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: GlobalStyles.colors.primary100,
+  },
+});
