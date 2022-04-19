bef4ab9c3fda8ce7ca2ff53245d68fa763f934d6
chicodevbr <chicodevbr@gmail.com>
2022-04-19 08:35:05 -0300

    saving coordinates

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Alert } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import { useState, useLayoutEffect, useCallback } from 'react';
+import IconButton from '../components/UI/IconButton';
 
-const Map = () => {
+const Map = ({ navigation }) => {
+  const [selectedLocation, setSelectedLocation] = useState();
   const region = {
     latitude: 37.78,
     longitude: -122.43,
@@ -9,7 +12,57 @@ const Map = () => {
     longitudeDelta: 0.0421,
   };
 
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+    setSelectedLocation({ lat: lat, lng: lng });
+  }
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectLocationHandler) {
+      Alert.alert(
+        'No location picked',
+        'You have to pick a location (by tapping on the map) first'
+      );
+      return;
+    }
+
+    navigation.navigate('AddPlace', {
+      pickedLat: selectedLocation.lat,
+      pickedLng: selectedLocation.lng,
+    });
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="save"
+          size={24}
+          color={tintColor}
+          onPress={savePickedLocationHandler}
+        />
+      ),
+    });
+  }, [navigation, savePickedLocationHandler]);
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 };
 
 export default Map;
