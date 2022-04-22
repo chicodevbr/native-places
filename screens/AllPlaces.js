bfc4919d9018b8fa87975e17febaa2bd93e68f50
chicodevbr <chicodevbr@gmail.com>
2022-04-22 08:42:52 -0300

    fetchin data sqlite

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,14 +1,22 @@
 import { useIsFocused } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
 import PlacesList from '../components/Places/PlacesList';
+import { fetchPlaces } from '../util/database';
 
 function AllPlaces({ route }) {
   const [loadedPlaces, setLoadedPlaces] = useState([]);
   const isFocused = useIsFocused();
 
   useEffect(() => {
-    if (isFocused && route.params) {
-      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    async function loadPlaces() {
+      const places = await fetchPlaces();
+      setLoadedPlaces(places);
+    }
+
+    if (isFocused) {
+      loadPlaces();
+
+      //setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
     }
   }, [isFocused, route]);
 
